0c70b02598d4d67f52c7efc2b95e58fac238bbd2
970024540 <970024540@qq.com>
2018-09-15 11:02:21 +0800

    Update Form.vue

--- src/views/nav1/Form.vue
+++ src/views/nav1/Form.vue
@@ -1,64 +1,102 @@
 <template>
-	<el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
-		<el-form-item label="活动名称">
-			<el-input v-model="form.name"></el-input>
-		</el-form-item>
-		<el-form-item label="活动区域">
-			<el-select v-model="form.region" placeholder="请选择活动区域">
-				<el-option label="区域一" value="shanghai"></el-option>
-				<el-option label="区域二" value="beijing"></el-option>
-			</el-select>
-		</el-form-item>
-		<el-form-item label="活动时间">
-			<el-col :span="11">
-				<el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-			</el-col>
-			<el-col class="line" :span="2">-</el-col>
-			<el-col :span="11">
-				<el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-			</el-col>
-		</el-form-item>
-		<el-form-item label="即时配送">
-			<el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
-		</el-form-item>
-		<el-form-item label="活动性质">
-			<el-checkbox-group v-model="form.type">
-				<el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-				<el-checkbox label="地推活动" name="type"></el-checkbox>
-				<el-checkbox label="线下主题活动" name="type"></el-checkbox>
-				<el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-			</el-checkbox-group>
-		</el-form-item>
-		<el-form-item label="特殊资源">
-			<el-radio-group v-model="form.resource">
-				<el-radio label="线上品牌商赞助"></el-radio>
-				<el-radio label="线下场地免费"></el-radio>
-			</el-radio-group>
-		</el-form-item>
-		<el-form-item label="活动形式">
-			<el-input type="textarea" v-model="form.desc"></el-input>
-		</el-form-item>
-		<el-form-item>
-			<el-button type="primary">立即创建</el-button>
-			<el-button @click.native.prevent>取消</el-button>
-		</el-form-item>
-	</el-form>
+	<div>
+		 <el-select style='margin-top:20px;' v-model="value" placeholder="请选择筛选条件">
+			<el-option
+			v-for="item in options"
+			:key="item.value"
+			:label="item.label"
+			:value="item.value">
+			</el-option>
+		</el-select>
+		<el-table
+			:data="tableData"
+			border
+			style="width: 100%;margin-top:20px;">
+			<el-table-column
+				prop="id"
+				label="编号">
+			</el-table-column>
+			<el-table-column
+				prop="table"
+				label="桌号">
+			</el-table-column>
+			<el-table-column
+				prop="vipId"
+				label="会员编号">
+			</el-table-column>
+			<el-table-column
+				prop="foodId"
+				label="菜品编号">
+			</el-table-column>
+			<el-table-column
+				prop="quanTity"
+				label="数量">
+			</el-table-column>
+			<el-table-column
+				prop="totalPrice"
+				label="价格">
+			</el-table-column>
+			<el-table-column
+				prop='orderDate'
+				label="日期">
+			</el-table-column>
+			<el-table-column
+			label="操作"
+			width="280">
+			<template slot-scope="scope">
+				<el-radio-group v-model="scope.row.status">
+					<el-radio label="initial">待确认</el-radio>
+					<el-radio label="making">制作中</el-radio>
+					<el-radio label="finished">已结单</el-radio>
+				</el-radio-group>
+			</template>
+			</el-table-column>
+
+		</el-table>
+	</div>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				form: {
-					name: '',
-					region: '',
-					date1: '',
-					date2: '',
-					delivery: false,
-					type: [],
-					resource: '',
-					desc: ''
-				}
+				value:'',
+				options:[
+					{
+						value:'initial',
+						label:'未完成'
+					},
+					{
+						value:'making',
+						label:'进行中'
+					},
+					{
+						value:'finished',
+						label:'已完成'
+					}
+				],
+				tableData:[
+					{
+						id:'1',
+						table:'1',
+						vipId:'65',
+						foodId:'27',
+						quanTity:'1',
+						totalPrice:'18￥',
+						orderDate:"2018/05/02",
+						status:'initial',
+					},
+					{
+						id:'2',
+						table:'3',
+						vipId:'21',
+						foodId:'14',
+						quanTity:'5',
+						totalPrice:'15￥',
+						orderDate:"2018/05/02",
+						status:'initial',
+					}
+				]
 			}
 		},
 		methods: {
